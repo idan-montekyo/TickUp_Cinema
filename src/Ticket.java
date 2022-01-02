721ef6672c32e90fdc07fb5f1b80d3ae7cff2b5b
niv19960 <niv19960@gmail.com>
2022-01-02 15:14:55 +0200

    Create Ticket Class

--- src/Ticket.java
+++ src/Ticket.java
@@ -5,23 +5,25 @@ public class Ticket {
 
     private HashMap<String, Integer> ticketsPrices = new HashMap<String, Integer>();
 
+    //Constructor - Initialize 3 types of tickets
     public Ticket() {
         this.ticketsPrices = ticketsPrices;
         this.ticketsPrices.put("Standard", 0);
-        this.ticketsPrices.put("Soldier", 0);
-        this.ticketsPrices.put("Cup", 0);
         this.ticketsPrices.put("Student", 0);
         this.ticketsPrices.put("Veteran", 0);
     }
 
+    // Get quantity of specific ticket type at specific order
     public int getNumOfTypeTickets(String type){
        return this.ticketsPrices.get(type);
     }
 
+    // Set quantity of specific ticket type for specific order
     public void SetNumOfTypeTickets(String type, int numOfTickets){
         this.ticketsPrices.put("Student", numOfTickets);
     }
 
+    // Display all quantity of all types of ticket at specific order
     public void displayAllNumOfTypeTickets(){
         for (String i : this.ticketsPrices.keySet()) {
             System.out.println("key: " + i + " value: " + this.ticketsPrices.get(i));
