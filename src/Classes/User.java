43e1232833925bc17b43b1e107371f2170f18a4d
niv19960 <niv19960@gmail.com>
2022-01-05 23:15:50 +0200

    Building User class

---
+++ src/Classes/User.java
@@ -0,0 +1,39 @@
+package Classes;
+
+public class User {
+    private boolean[][] theaterMatrix; // True - available, False - Occupied
+    int rows;
+    int cols;
+
+    // Constructor
+    public User(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        theaterMatrix = new boolean[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                this.theaterMatrix[row][col] = true;
+            }
+        }
+    }
+
+    // Display all movies
+    public void displayMoviesForSelection(Movies movies){
+        movies.displayMovies();
+    }
+
+    // Select a valid seat
+    public void selectSeat(int row, int col){
+        if(this.theaterMatrix[row][col] = true && (row < this.rows && this.rows > 0) && (col < this.cols && this.cols > 0)) {
+            this.theaterMatrix[row][col] = false;
+            System.out.println("Seats has selected!");
+        }
+        else {
+            System.out.println(("This seat is occupied or illegal!"));
+        }
+    }
+
+    public void selectTickets(UserOrder order){
+
+    }
+}
